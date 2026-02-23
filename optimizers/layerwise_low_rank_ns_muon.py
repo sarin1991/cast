cf62ec47410b1f5433fa2482b29f3a3c1410249b
Sarin Eapen Chandy <sarin1991@gmail.com>
2026-02-22 20:00:41 -0600

    Adding bf16 support

--- optimizers/layerwise_low_rank_ns_muon.py
+++ optimizers/layerwise_low_rank_ns_muon.py
@@ -98,8 +98,6 @@ class LowRankNSMuonOptimizer(torch.optim.Optimizer):
                     momentum_buffer_low_rank_init = torch.randn((r, gradient.shape[1]), device=p.device, dtype=torch.bfloat16)
                     state["momentum_buffer_low_rank"] = momentum_buffer_low_rank_init
                     momentum = torch.zeros_like(p,dtype=torch.bfloat16)
-                    if not isfloat32:
-                        state["low_rank_weight"] = torch.zeros_like(momentum_buffer_low_rank_init,dtype=torch.float32)
                 else:
                     momentum = decompress(state["projection_matrix"], state["momentum_buffer_low_rank"])
                 decay = (1 - group["lr"] * group["weight_decay"])
@@ -108,10 +106,7 @@ class LowRankNSMuonOptimizer(torch.optim.Optimizer):
                 if isfloat32:
                     W = p
                 else:
-                    W_lr_bf16 = state["projection_matrix"].T @ p
-                    p.addmm_(state["projection_matrix"], W_lr_bf16, beta=1.0, alpha=-1.0) # remove low precision lr weights
                     W = p.float() 
-                    W.addmm_( state["projection_matrix"].float(), state["low_rank_weight"], beta=decay, alpha=decay) #Add back high precision lr weights
                 # Update momentum
                 momentum.lerp_(gradient, 1 - beta)
                 update = gradient.lerp_(momentum, beta)
@@ -126,8 +121,6 @@ class LowRankNSMuonOptimizer(torch.optim.Optimizer):
                 W.add_(update.reshape(W.shape), alpha=-group["lr"])
                 if not isfloat32:
                     copy_stochastic_(p, W)
-                    # Recalculate low rank
-                    torch.matmul(state["projection_matrix"].T.float(), W, out=state["low_rank_weight"])
         return loss
 
 class LayerwiseLowRankNSMuonOptimizer(LowRankNSMuonOptimizer):
